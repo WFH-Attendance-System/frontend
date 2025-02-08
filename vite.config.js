4b4ebb725e04b43338a75284b156cfff50e86407
Chery <cherylene40@gmail.com>
2025-02-08 18:18:50 +0700

    Add exifreader and fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from 'path';
+import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
@@ -8,5 +8,8 @@ export default defineConfig({
         alias: {
             "@": path.resolve(__dirname, "src"),
         },
+    },
+    optimizeDeps: {
+        include: ["bootstrap"],
     },
 });
