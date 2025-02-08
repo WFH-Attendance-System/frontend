6a98680fd6be69986ca2dd6558454e5f5179e527
Chery <cherylene40@gmail.com>
2025-02-08 18:22:16 +0700

    Add logout page (without API)

---
+++ src/pages/authentication/Logout.jsx
@@ -0,0 +1,16 @@
+// import useAuth from "@/hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+const Logout = () => {
+    // const { logout } = useAuth();
+    const navigate = useNavigate();
+    useEffect(() => {
+        // logout();
+        navigate("/login");
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    return null;
+};
+
+export default Logout;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
 import Login from "@/pages/authentication/Login";
+import Logout from "@/pages/authentication/Logout";
 import Dashboard from "@/pages/Dashboard";
 import Error404 from "@/pages/error/Error404";
 import Employee from "@/pages/employee/Employee";
@@ -6,6 +7,7 @@ import Attendance from "@/pages/attendance/Attendance";
 
 export {
     Login,
+    Logout,
     Dashboard,
     Error404,
     Employee,
